5ffff4a4cd335ce74076f5c72785d8d306f4cb93
Bob O'Connor <roconnor@onwilsonpond.com>
2019-08-28 07:57:13 -0400

    Initial Commit

---
+++ TimersWithIntervalsCD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimersWithIntervalsCD
+//
+//  Created by Bob O'Connor on 8/28/19.
+//  Copyright © 2019 Bob O'Connor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimersWithIntervalsCDTests/TimersWithIntervalsCDTests.swift
@@ -0,0 +1,34 @@
+//
+//  TimersWithIntervalsCDTests.swift
+//  TimersWithIntervalsCDTests
+//
+//  Created by Bob O'Connor on 8/28/19.
+//  Copyright © 2019 Bob O'Connor. All rights reserved.
+//
+
+import XCTest
+@testable import TimersWithIntervalsCD
+
+class TimersWithIntervalsCDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
