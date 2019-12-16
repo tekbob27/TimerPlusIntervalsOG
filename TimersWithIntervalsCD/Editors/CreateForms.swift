44d42d3ae269b993a315ec900b47f95ed89caf2c
bob <tekbob27@gmail.com>
2019-12-16 06:25:17 -0500

    commit #2

--- TimersWithIntervalsCD/Editors/CreateForms.swift
+++ TimersWithIntervalsCD/Editors/CreateForms.swift
@@ -8,116 +8,65 @@
 
 import SwiftUI
 
-struct NewTimerForm: View {
-    @ObservedObject var timer: TempusBrevis
-    
-    var body: some View {
-        Form {
-            Section(header: Text("New")) {
-                TextField("Name", text: self.$timer.name)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                NavigationLink(destination: NewTimesForm(timer: timer)) {
-                    Text("New times").font(.headline).foregroundColor(.black)
-                }
-                NavigationLink(destination: NewSoundsForm(timer: timer)) {
-                    Text("New sounds").font(.headline).foregroundColor(.black)
-                }
-            }
-        }.frame(width: UIScreen.main.bounds.size.width, height: 225.0, alignment: .center)
-    }
-}
-
-struct NewTimesForm: View {
-    @ObservedObject var timer: TempusBrevis
-
-    @State var data: [[Int]] = [
-        Array(0...24),
-        Array(0...59),
-        Array(0...59)
-    ]
-    
-    var soundData = SoundFileData()
+struct TimesForm: View {
+    @ObservedObject var timer: TempusNova
 
     var body: some View {
-        Form {
-            Section(header: Text("Timer")) {
-                Text("Name: \(timer.name)")
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("Alarm: \(timer.toString("alarm"))")
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Text("Alert: \(timer.toString("interrupt"))")
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            Section(header: Text("Timer Duration: \(self.timer.toString("duration"))"),
-                    content: {
-                        HStack {
-                            VStack(alignment: .center, spacing: 0.0) {
-                                HStack(alignment: .bottom, spacing: 0.0) {
-                                    Spacer()
-                                    Text("Hours")
-                                    Spacer()
-                                    Text("Minutes")
-                                    Spacer()
-                                    Text("Seconds")
-                                    Spacer()
+        GeometryReader { geometry in
+            Form {
+                Section(header: HStack { Spacer()
+                    Text("Timer Duration:")
+                    Text("\(self.timer.duration.toString())").foregroundColor(Color.blue)
+                    Spacer() },
+                        content: {
+                            HStack(alignment: .center, spacing: 0) {
+                                VStack(alignment: .leading, spacing: 0.0) {
+                                    TimePicker(time: self.$timer.duration)
                                 }
-                                TempusEligo(time: self.$timer.duration)
-                            }
-                        }.frame(width: UIScreen.main.bounds.size.width, height: 200.0, alignment: .center)
-            })
-            Section(header: Text("Timer Interval: \(self.timer.interlude.toString())"),
-                    content: {
-                        HStack {
-                            VStack(alignment: .center, spacing: 0.0) {
-                                HStack(alignment: .bottom, spacing: 0.0) {
-                                    Spacer()
-                                    Text("Hours")
-                                    Spacer()
-                                    Text("Minutes")
-                                    Spacer()
-                                    Text("Seconds")
-                                    Spacer()
+                            }.frame(width: geometry.size.width * 0.95, height: 225.0, alignment: .center)
+                })
+                Section(header: HStack { Spacer()
+                Text("Timer Interval:")
+                Text("\(self.timer.interlude.toString())").foregroundColor(Color.blue)
+                Spacer() },
+                        content: {
+                            HStack(alignment: .center, spacing: 0) {
+                                VStack(alignment: .leading, spacing: 0.0) {
+                                    TimePicker(time: self.$timer.interlude, labels: ["HH", "MM"], displayValues: [Array(0...24), Array(0...59)])
                                 }
-                                TempusEligo(time: self.$timer.interlude)
-                            }
-                        }.frame(width: UIScreen.main.bounds.size.width, height: 200.0, alignment: .center)
-            })
+                            }.frame(width: geometry.size.width * 0.95, height: 225.0, alignment: .center)
+                })
+            }.frame(width: geometry.size.width, alignment: .center)
         }
     }
 }
 
-struct NewSoundsForm: View {
-    @ObservedObject var timer: TempusBrevis
-    @Environment(\.managedObjectContext) var managedObjectContext
-
-    @State var sounds: [[String]] = []
-
+struct SoundsForm: View {
+    @ObservedObject var timer: TempusNova
+    
     var body: some View {
-        Form {
-            Section(header: Text("Timer")) {
-                Text("Name: \(timer.name)")
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-            }
-            Section(header: Text("Alarm: \(self.timer.toString("alarm"))\t\tAlert: \(self.timer.toString("interrupt"))"),
-                    content: {
-                        HStack(alignment: .center, spacing: 0) {
-                            VStack(alignment: .center, spacing: 0) {
-                                HStack(alignment: .top, spacing: 0.0) {
-                                    Spacer()
-                                    Text("Alarms")
-                                    Spacer()
-                                    Text("Alerts")
-                                    Spacer()
-                                }
-                                PickerControl(data: $sounds,
-                                              selection: self.$timer.sounds)
-                            }
-                        }.frame(width: UIScreen.main.bounds.size.width, height: 225.0, alignment: .center)
-            })
-        }.onAppear() {
-            if self.sounds.count == 0 {
-                self.sounds.append(SoundFileData.Alarms)
-                self.sounds.append(SoundFileData.Alerts)
+        GeometryReader { geometry in
+            Form {
+                Section(header:
+                    ZStack(alignment: .leading) {
+                        Text("Current")
+                        HStack(alignment: .center) {
+                            Spacer()
+                            Text("\(self.timer.sound.alarm)").foregroundColor(Color.blue).font(.body)
+                            Spacer()
+                            Text("\(self.timer.sound.interrupt)").foregroundColor(Color.blue).font(.body)
+                            Spacer()
+                        }.background(Color.clear)
+                    },
+                        content: {
+                    HStack(alignment: .center, spacing: 0) {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Spacer()
+                            SoundPicker(sounds: self.$timer.sound, labels: ["Alarm", "Interrupt"])
+                            Spacer()
+                        }
+                    }.frame(width: geometry.size.width * 0.95, height: 225.0, alignment: .center)
+                })
             }
         }
     }
